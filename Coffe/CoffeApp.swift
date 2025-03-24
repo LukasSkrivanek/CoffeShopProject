b0ba8ccf86f9b502355f503deea9e1cc2cbb3d2f
LukasSkrivanek <luky.skrivos@gmail.com>
2025-03-24 11:25:40 +0100

    entrypoint from SwiftUI to UIKit

--- Coffe/CoffeApp.swift
+++ Coffe/CoffeApp.swift
@@ -4,36 +4,46 @@
 //
 //  Created by macbook on 26.02.2024.
 //
+import UIKit
 import SwiftUI
 import FirebaseCore
 
-@main
-struct CoffeApp: App {
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
-    var body: some Scene {
-        WindowGroup {
-            CoordinatorView()
-                .environment(DependencyContainer.resolve(IsDarkMode.self))
-                .environment(DependencyContainer.resolve(Coordinator.self))
-                .environment(DependencyContainer.resolve(AppState.self))
-                .environment(DependencyContainer.resolve(BasketViewModel.self))
-                .environment(DependencyContainer.resolve(DrinkListViewModel.self))
-                .environment(DependencyContainer.resolve(AccountViewModel.self))
-                .environment(DependencyContainer.resolve(RegistrationViewModel.self))
-                .environment(DependencyContainer.resolve(LoginViewModel.self))
-                .environment(DependencyContainer.resolve(LoginMethodSelectionViewModel.self))
-                .environment(DependencyContainer.resolve(UserRepository.self))
-                .environment(DependencyContainer.resolve(SecureStorage.self))
-        }
-    }
-}
-
-class AppDelegate: NSObject, UIApplicationDelegate {
+@UIApplicationMain
+final class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(
         _ application: UIApplication,
-        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
         FirebaseApp.configure()
         return true
+    }
+}
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        let contentView = CoordinatorView()
+            .environment(DependencyContainer.resolve(IsDarkMode.self))
+            .environment(DependencyContainer.resolve(Coordinator.self))
+            .environment(DependencyContainer.resolve(AppState.self))
+            .environment(DependencyContainer.resolve(BasketViewModel.self))
+            .environment(DependencyContainer.resolve(DrinkListViewModel.self))
+            .environment(DependencyContainer.resolve(AccountViewModel.self))
+            .environment(DependencyContainer.resolve(RegistrationViewModel.self))
+            .environment(DependencyContainer.resolve(LoginViewModel.self))
+            .environment(DependencyContainer.resolve(LoginMethodSelectionViewModel.self))
+            .environment(DependencyContainer.resolve(UserRepository.self))
+            .environment(DependencyContainer.resolve(SecureStorage.self))
+
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            window.rootViewController =  UIHostingController(rootView: contentView)
+            self.window = window
+            window.makeKeyAndVisible()
+        }
     }
 }
